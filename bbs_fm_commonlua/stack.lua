271d2bd0467cd870bd1d6091b32deb04ef38c46c
Luciano Ciccariello <xeeynamo@hotmail.com>
2020-09-06 15:11:13 +0200

    Fix stack.lua compilation error

--- bbs_fm_commonlua/stack.lua
+++ bbs_fm_commonlua/stack.lua
@@ -1,23 +1,24 @@
 Stack = {}
 Stack.__index = Stack
-function Stack.Create(_ARG_0_)
+
+function Stack.Create()
   for _FORV_6_ = 1, 64 do
-    table.insert({
-      _et = {},
-      ptr = 1
-    }._et, nil)
+    table.insert({}, nil)
   end
+
   return setmetatable({
     _et = {},
     ptr = 1
   }, Stack)
 end
+
 function Stack.push(_ARG_0_, _ARG_1_)
   if _ARG_1_ then
     _ARG_0_._et[_ARG_0_.ptr] = _ARG_1_
     _ARG_0_.ptr = _ARG_0_.ptr + 1
   end
 end
+
 function Stack.pop(_ARG_0_)
   if _ARG_0_.ptr > 1 then
     _ARG_0_.ptr = _ARG_0_.ptr - 1
@@ -26,6 +27,7 @@ function Stack.pop(_ARG_0_)
   end
   return nil
 end
+
 function Stack.get(_ARG_0_, _ARG_1_)
   if _ARG_0_.ptr > 1 then
     if _ARG_1_ ~= nil then
@@ -35,9 +37,11 @@ function Stack.get(_ARG_0_, _ARG_1_)
   end
   return nil
 end
+
 function Stack.getn(_ARG_0_)
   return _ARG_0_.ptr - 1
 end
+
 function Stack.isExist(_ARG_0_, _ARG_1_)
   for _FORV_6_ = 1, _ARG_0_.ptr do
     if _ARG_0_._et[_FORV_6_] == _ARG_1_ then
@@ -46,6 +50,7 @@ function Stack.isExist(_ARG_0_, _ARG_1_)
   end
   return _FOR_
 end
+
 function Stack.clear(_ARG_0_)
   for _FORV_5_ = 1, _ARG_0_.ptr do
     _ARG_0_._et[_FORV_5_] = nil
